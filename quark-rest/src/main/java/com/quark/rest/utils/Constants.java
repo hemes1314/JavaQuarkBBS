56481177ae70a5bb1082e48ce07fe3dec82f1693
wubb <wubb@zetyun.com>
2019-07-24 16:27:23 +0800

    upload folder update

--- quark-rest/src/main/java/com/quark/rest/utils/Constants.java
+++ quark-rest/src/main/java/com/quark/rest/utils/Constants.java
@@ -6,7 +6,7 @@ package com.quark.rest.utils;
  */
 public class Constants {
     //Upload常量
-    public static final String UPLOAD_PATH = "D:\\mnt\\images\\";
+    public static final String UPLOAD_PATH = "\\mnt\\images\\";
     public static final String STATIC_URL ="http://127.0.0.1/images/upload/";
 
 }
